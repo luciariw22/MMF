c46c51a31358c55b66566724881c9ada39f79b25
luciariw22 <125606190+luciariw22@users.noreply.github.com>
2024-02-13 09:25:49 +1300

    Add files via upload

---
+++ B_MM_base.py
@@ -0,0 +1,57 @@
+# functions go here
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+        else:
+            print("Please answer yes/no")
+
+
+# checks that user's name is not blank
+def not_blank(question):
+    while True:
+        response = input(question)
+
+        if response == "":
+            print("Sorry this can't be blank. Please try again")
+        else:
+            return response
+
+
+# main routine starts here
+
+# set maximum number of tickets
+MAX_TICKETS = 3
+tickets_sold = 0
+
+# Ask user if they want to see the instructions
+want_instructions = yes_no("Do you want to read the instructions? ")
+
+if want_instructions == "yes":
+    print("Instructions go here")
+
+print()
+
+# loop to sell tickets
+
+while tickets_sold < MAX_TICKETS:
+    name = not_blank("Enter your name (or 'xxx' to quit) ")
+
+    if name == 'xxx':
+        break
+
+    tickets_sold += 1
+
+# Output number of tickets sold
+if tickets_sold == MAX_TICKETS:
+    print("Congratulations you have sold all the tickets")
+else:
+    print("You have sold {} ticket/s. There is {} ticket/s "
+          "remaining".format(tickets_sold, MAX_TICKETS - tickets_sold))
