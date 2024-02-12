dfbc02a0161f438f361af117ba18543590439d8d
luciariw22 <125606190+luciariw22@users.noreply.github.com>
2024-02-13 09:45:25 +1300

    Add files via upload

--- B_MM_base.py
+++ B_MM_base.py
@@ -24,6 +24,17 @@ def not_blank(question):
         else:
             return response
 
+# checks user enters an integer to the question
+def num_check(question):
+
+    while True:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print("Please enter an integer.")
 
 # main routine starts here
 
@@ -46,6 +57,17 @@ while tickets_sold < MAX_TICKETS:
 
     if name == 'xxx':
         break
+
+    age = num_check("Age: ")
+
+    if 12 <= age <= 120:
+        pass
+    elif age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    else:
+        print("?? That looks like a typo, please try again.")
+        continue
 
     tickets_sold += 1
 
